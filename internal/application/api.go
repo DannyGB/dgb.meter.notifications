0cf41d33dee7c6167ac45b31d7033f08986a8ad7
dannygb <dangbeavon@gmail.com>
2023-01-24 16:56:25 +0000

    Adds content type header to /ping

--- internal/application/api.go
+++ internal/application/api.go
@@ -18,6 +18,7 @@ func HandleRequests(conf configuration.Configuration) {
 
 	subRoute.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
+		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode("OK")
 	}).Methods(http.MethodGet)
 
